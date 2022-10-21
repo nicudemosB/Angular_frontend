dfe3f6dcd3ea59283ede3c3c4d6b6a394bb5e590
Billy <billyn0213@gmail.com>
2022-10-20 23:46:36 -0400

    Login User Auth is fully functional

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/User';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +10,29 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class HeaderComponent implements OnInit {
   cartQuantity=0
-  constructor(cartService:CartService) {
+  user!:User
+  constructor(cartService:CartService, private userService:UserService) {
     cartService.getCartObservable().subscribe((newCart) => {
       this.cartQuantity = newCart.totalCount
+    })
+
+    userService.userObservable.subscribe((newUser) => {
+    })
+
+    userService.userObservable.subscribe((newUser) => {
+      this.user = newUser
     })
    }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.userService.logout()
+  }
+// this means that the user is authenticated
+  get isAuth() {
+    return this.user.token
   }
 
 }
--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -6,4 +6,6 @@ export const FOODS_BY_SEARCH_URL = FOODS_URL +'/search/'
 // the slash / after search lets you pass the route parameter and search gets passed more easily
 export const FOODS_BY_TAG_URL = FOODS_URL +'/tag/'
 export const FOODS_BY_ID_URL = FOODS_URL +'/'
+// this is the url that we defined in our server, if you define something else, you need to use the address below
+export const USER_LOGIN_URL = BASE_URL + '/api/users/login'
 
